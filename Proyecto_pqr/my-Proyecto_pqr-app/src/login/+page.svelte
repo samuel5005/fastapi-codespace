622486adc44051ca2af30b7a8c926cba9853423f
1002160932j-create <1002160932j@gmail.com>
2026-03-12 19:05:12 +0000

    nuevo cambio

---
+++ Proyecto_pqr/my-Proyecto_pqr-app/src/login/+page.svelte
@@ -0,0 +1,47 @@
+<script>
+	let correo = "";
+	let password = "";
+	let mensaje = "";
+
+	async function iniciarSesion() {
+
+		const respuesta = await fetch("http://localhost:8000/login", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json"
+			},
+			body: JSON.stringify({
+				correo: correo,
+				password: password
+			})
+		});
+
+		const data = await respuesta.json();
+
+		mensaje = data.mensaje;
+
+		if (data.usuario) {
+			alert("Bienvenido");
+		}
+	}
+</script>
+
+<h1>Login</h1>
+
+<input 
+	type="email" 
+	placeholder="Correo"
+	bind:value={correo}
+/>
+
+<input 
+	type="password"
+	placeholder="Contraseña"
+	bind:value={password}
+/>
+
+<button on:click={iniciarSesion}>
+	Iniciar sesión
+</button>
+
+<p>{mensaje}</p>
